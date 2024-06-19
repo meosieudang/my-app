1001eee51e19c7af545b3462c6e9d9e64a5afcf2
thuanhn <thuanhn@creasia.com.vn>
2024-06-19 15:48:18 +0700

    init source

---
+++ src/@service/lucky_wheel_config.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const getLuckyWheelPlanConfigs = (id, campaignId) => {
+    let params = {
+        id, campaignId
+    };
+    return axios.get("api/luckywheelconfig/get_luckywheelplanconfigs", { params });
+}
+
+const getInfoLuckyWheel = () => {
+    return axios.get("api/luckywheelconfig/get_infoluckywheel");
+}
+const createLuckyWheelPlanConfigs = (data) => {
+    return axios.post("api/luckywheelconfig/create_luckywheelplanconfigs", data);
+}
+
+const updateLuckyWheelPlanConfigs = (data) => {
+    return axios.post("api/luckywheelconfig/update_luckywheelplanconfigs", data);
+}
+
+const updateLuckyWheelGifts = (data) => {
+    return axios.post("api/luckywheelconfig/update_luckywheelgifts", data);
+}
+const getLuckyWheel_noofwheel = (orderCode) => {
+    let params = {
+        orderCode
+    };
+    return axios.get("api/luckywheel/get_luckywheel_noofwheel", { params });
+}
+
+const LuckyWheelConfigServices = {
+    getLuckyWheelPlanConfigs,
+    getInfoLuckyWheel,
+    createLuckyWheelPlanConfigs,
+    updateLuckyWheelPlanConfigs,
+    updateLuckyWheelGifts,
+    getLuckyWheel_noofwheel
+}
+
+export default LuckyWheelConfigServices;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+const CBVNLuckyDraw2024 = lazy(() =>
+  import("./Cbvn_2024").then((m) => ({ default: m.CBVNLuckyDraw2024 }))
+);
+const Load = ({ View, ...props }) => (
+  <Suspense fallback={<div></div>}>{View && <View {...props} />}</Suspense>
+);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Load View={CBVNLuckyDraw2024} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
