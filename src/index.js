378b4899c2605719ffddcef6baf8cce74be98548
thuanhn <thuanhn@creasia.com.vn>
2024-06-19 17:06:13 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./style.css";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./style.css";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { lazy } from "react";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// // reportWebVitals();
+// // export { default as CustomButton } from "../src/CustomButton";
+const CBVNLuckyDraw2024 = lazy(() =>
+  import("./Cbvn_2024").then((m) => ({ default: m.CBVNLuckyDraw2024 }))
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-// export { default as CustomButton } from "../src/CustomButton";
+export { CBVNLuckyDraw2024 };
